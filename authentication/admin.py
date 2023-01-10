b971ddf537da7a38b86f66b24326eefbcd6c7d5a
SodokinMarius <yaomariussodokin@gmail.com>
2023-01-10 18:46:24 +0100

    feat : add model to admin site

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import User
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    list_display=('username','full_name','password')
--- todoListApp/admin.py
+++ todoListApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import Task
+@admin.register(Task)
+class UserAdmin(DefaultUserAdmin):
+    list_display=('title','description','progress_status')
+    
